aed566a2dd79db3f4ae97ba7d9c6c39d40457fe5
Gauri Khandke <khandkegauri@gmail.com>
2020-09-10 17:20:22 -0700

    Added database schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,19 @@
 --     * **id**: an auto incrementing int that serves as the primary key.
 --     * **burger_name**: a string.
 --     * **devoured**: a boolean.
+
+-- Drops/deletes database if already exists -- 
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Creates the "burgers_db" database --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create table 'burgers'
+CREATE TABLE burgers(
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY(id)
+);

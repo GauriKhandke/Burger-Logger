26369b512f149b7b16079223af58bb72385983b3
Gauri Khandke <khandkegauri@gmail.com>
2020-09-11 20:39:41 -0700

    Created model to interact with database

--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,35 @@
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
 // Export at the end of the burger.js file.
+
+var orm = require("../config/orm");
+const connection = require("../config/connection");
+
+var burger = {
+
+    selectAll : function(cb){
+
+        orm.selectAll("burgers", function(result){
+            cb(result);
+        });
+    },
+
+    add : function(cols, vals, cb){
+
+        orm.insertOne("burgers",cols, vals, cb, function(result){
+            cb(result);
+        });
+
+    },
+
+    update : function(vals, condition, cb){
+
+        orm.updateOne("burgers", vals, condition, cb, function(result){
+            cb(result);
+        });
+    }
+
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;

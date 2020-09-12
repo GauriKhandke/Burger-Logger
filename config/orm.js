b9e7a1b52190e8356f6b3347d7f1018c2ba3a618
Gauri Khandke <khandkegauri@gmail.com>
2020-09-11 20:38:17 -0700

    Created ORM queries and functions

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,5 @@
 // Import (require) connection.js into orm.js
+var connection = require("../config/connection");
 
 // In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. 
 // These are the methods you will need to use in order to retrieve and store data in your database.
@@ -7,3 +8,73 @@
 // insertOne()
 // updateOne()
 // Export the ORM object in module.exports.
+
+
+// Helper function for SQL syntax.
+// If we want to pass 3 values into the mySQL query. In order to write the query, we need 3 question marks.
+// This helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    let arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+}
+
+// Object for all SQL statement functions.
+var orm = {
+
+    selectAll : function(tableName, cb){
+        
+        let queryString = `SELECT * FROM ${tableName};`;
+        
+        connection.query(queryString, function(err,result){
+            if(err) throw err;
+            cb(result);
+        });
+    },
+
+    insertOne : function(table, cols, vals, cb){
+
+        let queryString = `INSERT INTO ${table} 
+        (${cols.toString()})
+        VALUES (${printQuestionMarks(vals.length)});   
+        `;
+
+        console.log(`insertOne Query : ${queryString}`);
+
+        connection.query(queryString, vals, function(err, result){
+
+            if(err) throw err;
+
+            cb(result);
+        });
+
+    },
+
+    updateOne : function(table, vals, condition, cb){
+
+        let queryString = `
+        UPDATE ${table}
+        SET ?
+        WHERE
+        ${condition};
+        `;
+
+        console.log(`UpdateOne Query : ${queryString}`);
+
+        connection.query(queryString, [vals], function(err, result){
+
+            if(err) throw err;
+
+            cb(result);
+        });
+    }
+
+};
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
